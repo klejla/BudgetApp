3a4389fa9dc0a8d928e59e8656ee6954dcdf5aa6
klejla <57046260+klejla@users.noreply.github.com>
2020-10-25 14:03:36 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Image, SafeAreaView } from "react-native";
+import HomeScreeen from "./app/screens/HomeScreen";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+
+export default function App() {
+  return <WelcomeScreen />;
+}
+
+
